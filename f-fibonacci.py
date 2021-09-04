cd90c03127452ca716ab5c7c4b5be8b018e546e2
Abhishek Muley <73030681+amuley11@users.noreply.github.com>
2021-09-04 10:40:16 +0800

    Update f-fibonacci.py

--- f-fibonacci.py
+++ f-fibonacci.py
@@ -7,16 +7,19 @@ def show_fibonacci(event, context):
     This is a function to generate a fibonacci series till the position entered by the user
     '''
     fibonacci_list = []
-    counter = 2
-    
+
     while len(fibonacci_list) < num_position:
-        if counter == 2:
+        if len(fibonacci_list) < 1:
             fibonacci_list.append(0)
+        elif len(fibonacci_list) < 2:
             fibonacci_list.append(1)
         else:
             fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
-        counter += 1
 
-    response = 'Fibonacci series till position ' + str(num_position) + " - " + str(fibonacci_list).strip('[]')
+    if num_position == 0:
+        response = "Since you have entered 0, fibonacci series couldn't be generated."
+    else:
+        response = 'Fibonacci series till position ' + str(num_position) + " - " + str(fibonacci_list).strip('[]')
+    
     print(response)
     return response
